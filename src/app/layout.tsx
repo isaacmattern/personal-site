c8137883f3e754a83021bd3665677a4fdcc0a74f
Isaac Mattern <isaac.mattern@gmail.com>
2023-09-19 23:24:59 -0400

    disable x overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='overflow-x-hidden'>
       <body className={inter.variable}>
         <div className='mx-auto max-w-3xl'>
 
