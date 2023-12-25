5e933fd68961bb7638afefb32fb46d02e14b3f3d
Isaac Mattern <isaac.mattern@gmail.com>
2023-12-25 16:23:35 -0500

    increase blockquote padding

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Test() {
-  return <button onClick={() => console.log('clicked')}>Click Me</button>
+  const [count, setCount] = useState(0);
+  return (
+    <button
+      className="bg-white p-2 text-black font-bold m-auto block rounded-md"
+      onClick={() => {
+        setCount(count + 1);
+        console.log(`clicked ${count + 1} times`);
+      }}
+    >
+      {"Clicked " + count + " times"}
+    </button>
+  );
 }
