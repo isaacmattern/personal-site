792c234e7d2b5c500eab142a80e03cb4f8deaf12
Isaac Mattern <isaac.mattern@gmail.com>
2025-04-21 21:02:48 -0400

    update postcard date

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -2,37 +2,16 @@ import { Post } from "contentlayer/generated";
 import Link from "next/link";
 import "./PostCard.css";
 
-function formatDate(date) {
-  const day = String(date.getDate()).padStart(2, "0");
-  const monthAbbr = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthAbbr[date.getMonth()];
-  const year = date.getFullYear();
-
-  return `${day} ${month} ${year}`;
-}
-
-// Example usage:
-const date = new Date("2024-03-08");
-console.log(formatDate(date)); // Output: "08 Mar 2024"
-
 export default function PostCard(post: Post) {
   return (
     <div className="postcard">
       <time className="secondary-text" dateTime={post.date}>
-        {formatDate(new Date(post.date))}
+        {new Date(post.date).toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "short", // or "long" if you want full month names
+          year: "numeric",
+          timeZone: "UTC",
+        })}
       </time>
 
       <div>
