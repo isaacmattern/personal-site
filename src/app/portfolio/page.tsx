63e303ee3de8ad1e7c07e8e1ffeaaa9796b4a748
Isaac Mattern <isaac.mattern@gmail.com>
2025-05-03 09:20:07 -0400

    create about me page, clean up blog

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <Background image="/bg-images/default.gif">
+    <Background image="/bg-images/clouds.gif">
       <div className="portfolio-page-container">
         <div className="header-card">
           <h1>{"Portfolio"}</h1>
@@ -19,12 +19,7 @@ export default function Page() {
         <div className="sticky-note-container">
           {personalProjects.map((project, key) => {
             return (
-              <Link
-                key={key}
-                target="_blank"
-                href={project.url}
-                className="sticky-note"
-              >
+              <Link key={key} target="_blank" href={project.url}>
                 <h2>{project.title}</h2>
                 <p>{project.description}</p>
               </Link>
