83c1167b061212dd8ff1c0fea2a321e6fc0ccd70
Isaac Mattern <isaac.mattern@gmail.com>
2025-05-06 19:19:29 -0400

    edit homepage and portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,27 +1,34 @@
 import { personalProjects } from "@/lib/personalProjects";
 import "./portfolio.css";
 import Link from "next/link";
+import parse from "html-react-parser";
 
 export default function Page() {
   return (
     <body className="bg-portfolio">
       <main className="portfolio-page-container">
         <div className="header-card">
-          <h1>{"Portfolio"}</h1>
-          <p>
-            {
-              "Here are some cool projects that I've either done for fun in my free time or have completed for other people. Click on a card to view the project!"
-            }
-          </p>
+          <div>
+            <h1>{"Portfolio"}</h1>
+            <p>
+              {
+                "Here are some cool projects that I've either done for fun in my free time or have completed for other people."
+              }
+            </p>
+          </div>
         </div>
 
         <div className="sticky-note-container">
           {personalProjects.map((project, key) => {
             return (
-              <Link key={key} target="_blank" href={project.url}>
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
-              </Link>
+              <div key={key}>
+                <h2>
+                  <Link target="_blank" href={project.url}>
+                    {project.title}
+                  </Link>
+                </h2>
+                <p>{parse(project.description)}</p>
+              </div>
             );
           })}
         </div>
