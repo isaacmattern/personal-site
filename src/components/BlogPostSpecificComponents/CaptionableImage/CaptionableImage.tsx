7054096e37e0c9325b73affcef274cf5e85bcd67
Isaac Mattern <isaac.mattern@gmail.com>
2025-05-06 20:15:05 -0400

    add pausable gif post

--- src/components/BlogPostSpecificComponents/CaptionableImage/CaptionableImage.tsx
+++ src/components/BlogPostSpecificComponents/CaptionableImage/CaptionableImage.tsx
@@ -5,17 +5,19 @@ interface CaptionableImagePropsType {
   src: string;
   alt: string;
   caption?: string;
+  maxWidth?: number;
 }
 
 export default function CaptionableImage({
   src,
   alt,
   caption,
+  maxWidth,
 }: CaptionableImagePropsType): React.ReactElement {
   return (
     <div className="captionable-image">
       <figure>
-        <Image src={src} alt={alt} width={9999} height={100} />
+        <Image src={src} alt={alt} width={maxWidth || 9999} height={100} />
         {caption && (
           <figcaption className="secondary-text">{caption}</figcaption>
         )}
--- src/components/MdxComponents.tsx
+++ src/components/MdxComponents.tsx
@@ -1,12 +1,22 @@
 import { MDXComponents } from "mdx/types";
 import Link from "next/link";
-import CaptionableImage from "./CaptionableImage/CaptionableImage";
+import CaptionableImage from "./BlogPostSpecificComponents/CaptionableImage/CaptionableImage";
 import Test from "./Test/Test";
+import HomepageLink from "./HomepageLinks/HomepageLink";
+import {
+  PausableSaladTossGif,
+  PausableSaladTossGifWithOutline,
+  PausableSaladTossGifWithPermanentOutline,
+} from "./BlogPostSpecificComponents/PausableSaladTossGif/PausableSaladTossGif";
 /**
  * Add components here for use within .mdx files
  */
 export const mdxComponents: MDXComponents = {
   CaptionableImage,
+  HomepageLink,
+  PausableSaladTossGif,
+  PausableSaladTossGifWithOutline,
+  PausableSaladTossGifWithPermanentOutline,
   Test,
   a: ({ href, children }) => (
     <Link target="_blank" href={href as string}>
