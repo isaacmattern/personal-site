e96829d94ca9ef4aad2887e3fed471108e06a591
Isaac Mattern <isaac.mattern@gmail.com>
2025-11-09 17:11:54 -0500

    update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="not-found-container">
+    <body className="not-found-container bg-not-found">
       <h1>{"Page not found"}</h1>
       <p>{"The page you're looking for doesn't exist."}</p>
       <Image src={"/gifs/mole.gif"} height={200} width={200} alt="happy mole" />
-    </div>
+    </body>
   );
 }
