2b7fb4dc0ace4f565eadfd0a93831c267d25e919
Isaac Mattern <isaac.mattern@gmail.com>
2025-11-09 17:31:28 -0500

    Remove rehype and rehype pretty code

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,5 @@
 // contentlayer.config.ts
 import { defineDocumentType, makeSource } from 'contentlayer2/source-files'
-import rehypePrettyCode from 'rehype-pretty-code'
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -34,36 +33,5 @@ export default makeSource({
   contentDirPath: 'src',
   documentTypes: [Post, Recipe],
   mdx: {
-    rehypePlugins: [
-      [
-        // @ts-ignore
-        rehypePrettyCode,
-        {
-          theme: {
-            dark: 'dark-plus',
-            light: 'github-light-high-contrast',
-          },
-          keepBackground: false,
-          defaultLang: {
-            block: 'plaintext',
-            inline: 'plaintext',
-          },
-          onVisitLine(node: any) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }];
-            }
-          },
-          onVisitHighlightedLine(node: any) {
-            node.properties.className? node.properties.className.push("line--highlighted") : node.properties.className = "line--highlighted";
-          },
-          // Not currently used for anything.
-          onVisitHighlightedWord(node: any) {
-            node.properties.className? node.properties.className.push("word--highlighted") : node.properties.className = "word--highlighted";
-          },
-        },
-      ],
-    ],
   },
 })
